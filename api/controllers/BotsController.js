32361c4fa81c287dc01f88d09366d96e9327cbb6
Chintan <chintan@wohlig.com>
2018-02-14 18:27:38 +0530

    ok

--- api/controllers/BotsController.js
+++ api/controllers/BotsController.js
@@ -1,9 +1,9 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-const baseUrl = 'https://api.dialogflow.com/v1/';
+var baseUrl = 'https://api.dialogflow.com/v1/';
 var controller = {
     chatmsgs: function (req, res) {
         console.log("inside chat");
-        response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+        response = "This is a sample response from your webhook!"; //Default response from the webhook to show it's working
         res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
         res.send(JSON.stringify({
             "speech": response,
@@ -12,7 +12,7 @@ var controller = {
         }));
     },
     searchText: function (req, res) {
-        console.log("query", req.body.query)
+        console.log("query", req.body.query);
         var radius = 16000;
         var sensor = false;
         var types = "restaurant";
@@ -27,7 +27,7 @@ var controller = {
             "query": req.body.query,
             "sessionId": "12345",
             "timezone": "America/New_York"
-        }
+        };
         var options = {
             method: 'post',
             body: queryBody, // Javascript object
@@ -37,20 +37,20 @@ var controller = {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer 6a45d07194ea44deafe1da15f020c71b'
             }
-        }
+        };
 
         request(options, function (err, response1, body) {
             if (err) {
                 console.log('Error :', err);
             } else {
-                console.log(' Body :', body.result.parameters)
+                console.log(' Body :', body.result.parameters);
                 parameters.types = body.result.parameters['type-of-locations'];
                 parameters.sensor = false;
                 parameters.radius = 16000;
                 parameters.location = body.result.parameters['geo-city'];
 
-                parameters.query = parameters.types + ' in ' + parameters.location
-                var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + parameters.query + '&key=AIzaSyC2cMB4K6lnmacErJtGEBOJpJoNpZW1JIw'
+                parameters.query = parameters.types + ' in ' + parameters.location;
+                var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + parameters.query + '&key=AIzaSyC2cMB4K6lnmacErJtGEBOJpJoNpZW1JIw';
                 console.log(url);
                 https.get(url, function (response) {
                     var body = '';
@@ -74,7 +74,7 @@ var controller = {
 
     },
     saveText: function (req, res) {
-        console.log("query", req.body.text)
+        console.log("query", req.body.text);
         var radius = 16000;
         var sensor = false;
         var types = "restaurant";
@@ -89,7 +89,7 @@ var controller = {
             "query": req.body.text,
             "sessionId": "12345",
             "timezone": "America/New_York"
-        }
+        };
         var options = {
             method: 'post',
             body: queryBody, // Javascript object
@@ -99,7 +99,7 @@ var controller = {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer 6a45d07194ea44deafe1da15f020c71b'
             }
-        }
+        };
         request(options, function (err, response1, body) {
             if (err) {
                 console.log('Error :', err);
@@ -108,24 +108,24 @@ var controller = {
                     data: {
                         message: "Invalid Request"
                     }
-                })
+                });
             } else {
                 var botData = {};
                 botData.text = req.body.text;
                 botData.user = req.body.user;
                 if (body.result.parameters) {
-                    console.log(' Body :', body.result.parameters)
-                    botData.intent = body.result.parameters
+                    console.log(' Body :', body.result.parameters);
+                    botData.intent = body.result.parameters;
                 }
                 if (req.body.text.includes('Hey Alexa')) {
-                    console.log(' Body :', body.result.parameters)
+                    console.log(' Body :', body.result.parameters);
                     parameters.types = body.result.parameters['type-of-locations'];
                     parameters.sensor = false;
                     parameters.radius = 16000;
                     parameters.location = body.result.parameters['geo-city'];
 
-                    parameters.query = parameters.types + ' in ' + parameters.location
-                    var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + parameters.query + '&key=AIzaSyC2cMB4K6lnmacErJtGEBOJpJoNpZW1JIw'
+                    parameters.query = parameters.types + ' in ' + parameters.location;
+                    var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + parameters.query + '&key=AIzaSyC2cMB4K6lnmacErJtGEBOJpJoNpZW1JIw';
                     console.log(url);
                     https.get(url, function (response) {
                         var body = '';
@@ -160,7 +160,7 @@ var controller = {
                         data: {
                             message: "Invalid Request"
                         }
-                    })
+                    });
                 } else {
                     res.callback(null, "Removed");
                 }
--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -9,6 +9,26 @@ myApp.factory('apiService', function ($http, $q, $timeout) {
                 data: formData
             }).success(callback);
         },
+        saveText: function (text, callback) {
+            $http({
+                url: adminurl + 'bots/saveText',
+                method: 'POST',
+                data: {
+                    user: $.jStorage.get("name"),
+                    text: text
+                }
+            }).success(callback);
+        },
+        clearText: function (text, callback) {
+            $http({
+                url: adminurl + 'bots/clearText',
+                method: 'POST',
+                data: {
+                    user: $.jStorage.get("name"),
+                    text: "clear all"
+                }
+            }).success(callback);
+        }
         // This is a demo Service for POST Method.
 
 
