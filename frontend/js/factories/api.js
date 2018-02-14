a0169b9a12ceaadf7de8ca14ac42d82eaa572b39
Chintan <chintan@wohlig.com>
2018-02-14 18:48:25 +0530

    ko

--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -1,13 +1,6 @@
 myApp.factory('apiService', function ($http, $q, $timeout) {
     return {
         // This is a demo Service for POST Method.
-        getDemo: function (formData, callback) {
-            $http({
-                url: adminurl + 'demo/demoService',
-                method: 'POST',
-                data: formData
-            }).success(callback);
-        },
         saveText: function (text, callback) {
             $http({
                 url: adminurl + 'bots/saveText',
@@ -19,6 +12,16 @@ myApp.factory('apiService', function ($http, $q, $timeout) {
             }).success(callback);
         },
         clearText: function (text, callback) {
+            $http({
+                url: adminurl + 'bots/clearText',
+                method: 'POST',
+                data: {
+                    user: $.jStorage.get("name"),
+                    text: "clear all"
+                }
+            }).success(callback);
+        },
+        getAll: function () {
             $http({
                 url: adminurl + 'bots/clearText',
                 method: 'POST',
