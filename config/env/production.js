88f88747bd349dbba9badbd4f8476406e43a0e97
Chintan <chintan@wohlig.com>
2018-02-15 16:21:33 +0530

    ok

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 89,
-  realHost: "http://bot.wohlig.co.in",
+  realHost: "https://bot.wohlig.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
